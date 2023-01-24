85f33a40f0deb9d01d14214cac81a7b06cd0fa11
Donald Ajaps <ajapon@yahoo.com>
2023-01-24 22:40:48 +0100

    Trim sort.h

--- sort.h
+++ sort.h
@@ -8,8 +8,8 @@
  * struct listint_s - Doubly linked list node
  *
  * @n: Integer stored in the node
- * @prev: Pointer to the previous element of the list
- * @next: Pointer to the next element of the list
+ * @prev: Pointer to the previous element in the list
+ * @next: Pointer to the next element in the list
  */
 typedef struct listint_s
 {
@@ -17,22 +17,5 @@ typedef struct listint_s
 	struct listint_s *prev;
 	struct listint_s *next;
 } listint_t;
-
-void print_array(const int *array, size_t size);
-void print_list(const listint_t *list);
-void bubble_sort(int *array, size_t size);
-void insertion_sort_list(listint_t **list);
-void selection_sort(int *array, size_t size);
-void quick_sort(int *array, size_t size);
-void quick_sort_hoare(int *array, size_t size);
-void _qsort(int *a, int low, int high, int size);
-void shell_sort(int *array, size_t size);
-void _ssort(int *a, int size, int n);
-void cocktail_sort_list(listint_t **list);
-void counting_sort(int *array, size_t size);
-void merge_sort(int *array, size_t size);
-void heap_sort(int *array, size_t size);
-void radix_sort(int *array, size_t size);
-void bitonic_sort(int *array, size_t size);
 
 #endif /* _SORT_H_ */
