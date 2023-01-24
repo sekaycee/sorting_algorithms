2dce7bfa254c8c3451efd635a5fd01266adacf54
Kay Cee <se.kaycee@gmail.com>
2023-01-24 12:41:32 +0100

    edit sort.h

--- sort.h
+++ sort.h
@@ -24,6 +24,7 @@ void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
+void quick_sort_hoare(int *array, size_t size);
 void _qsort(int *a, int low, int high, int size);
 void shell_sort(int *array, size_t size);
 void _ssort(int *a, int size, int n);
