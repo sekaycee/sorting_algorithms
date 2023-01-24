316aebd7a701549a4bf8e2b012fa8a038aaf73fd
Donald Ajaps <ajapon@yahoo.com>
2023-01-24 22:43:42 +0100

    Undo trim of sort.h

--- sort.h
+++ sort.h
@@ -18,4 +18,21 @@ typedef struct listint_s
 	struct listint_s *next;
 } listint_t;
 
+void print_array(const int *array, size_t size);
+void print_list(const listint_t *list);
+void bubble_sort(int *array, size_t size);
+void insertion_sort_list(listint_t **list);
+void selection_sort(int *array, size_t size);
+void quick_sort(int *array, size_t size);
+void quick_sort_hoare(int *array, size_t size);
+void _qsort(int *a, int low, int high, int size);
+void shell_sort(int *array, size_t size);
+void _ssort(int *a, int size, int n);
+void cocktail_sort_list(listint_t **list);
+void counting_sort(int *array, size_t size);
+void merge_sort(int *array, size_t size);
+void heap_sort(int *array, size_t size);
+void radix_sort(int *array, size_t size);
+void bitonic_sort(int *array, size_t size);
+
 #endif /* _SORT_H_ */
